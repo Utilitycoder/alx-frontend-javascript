83259ab547d121747737318a0ec27a79d33f7de6
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-07-26 20:58:19 +0100

    chore: refactored

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,11 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-    try {
-        const photo = await uploadPhoto();
-        const user = await createUser();
-        return { photo, user };
-    } catch (err) {
-        return { photo: null, user: null };
-    }
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return { photo, user };
+  } catch (err) {
+    return { photo: null, user: null };
+  }
 }
