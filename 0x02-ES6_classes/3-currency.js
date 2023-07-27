283f417554fb0d51d5da3a6d52e7beb4e01ba976
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-07-27 17:08:51 +0100

    chore: implemented a Pricing class that builds upon the currency class

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,35 +1,35 @@
 export default class Currency {
-    constructor(code, name) {
-        this._code = code;
-        this._name = name;
-    }
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
 
-    // Code
-    get code() {
-        return this._code;
-    }
+  // Code
+  get code() {
+    return this._code;
+  }
 
-    set code(newCode) {
-        if (typeof newCode !== 'string') {
-            throw TypeError('Code must be a string');
-        }
-        this._code = newCode;
+  set code(newCode) {
+    if (typeof newCode !== 'string') {
+      throw TypeError('Code must be a string');
     }
+    this._code = newCode;
+  }
 
-    // Name
-    get name() {
-        return this._name;
-    }
+  // Name
+  get name() {
+    return this._name;
+  }
 
-    set name(newName) {
-        if (typeof newName !== 'string') {
-            throw TypeError('Name must be a string');
-        }
-        this._name = newName;
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw TypeError('Name must be a string');
     }
+    this._name = newName;
+  }
 
-    // Display
-    displayFullCurrency() {
-        return `${this._name} (${this._code})`;
-    }
+  // Display
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }
