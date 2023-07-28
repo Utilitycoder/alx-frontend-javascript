e8c14fad90222d8a5e203668afb054d3ec0b7af1
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-07-28 15:19:44 +0100

    refactored

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,7 @@
 export default function iterateThroughObject(reportWithIterator) {
-    let employees = '';
-    for (const employee of reportWithIterator) {
-        employees += `${employee} | `;
-    }
-    return employees.slice(0, -3);
+  let employees = '';
+  for (const employee of reportWithIterator) {
+    employees += `${employee} | `;
+  }
+  return employees.slice(0, -3);
 }
