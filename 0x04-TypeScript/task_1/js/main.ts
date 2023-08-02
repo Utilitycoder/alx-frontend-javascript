92a313e17bc24b478285349c598613a83b473dfa
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-08-02 17:13:37 +0100

    chore: created more tasks

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,12 +1,12 @@
-interface ITeacher {
-    firstName: string;
-    lastName: string;
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
     fullTimeEmployee: boolean;
     yearsOfExperience?: number;
     location: string;
-}
+};
 
-const teacher1: ITeacher = {
+const teacher1: Teacher = {
     firstName: 'Lawal',
     lastName: 'Abubakar',
     fullTimeEmployee: true,
@@ -14,4 +14,42 @@ const teacher1: ITeacher = {
     location: 'Lagos'
 };
 
-console.log(teacher1);
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+const director1: Directors = {
+    firstName: 'Mike',
+    lastName: 'Okafor',
+    fullTimeEmployee: true,
+    yearsOfExperience: 10,
+    location: 'Abuja',
+    numberOfReports: 4
+};
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+export const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+    return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+class StudentClass {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework() {
+        return 'Currently working';
+    }
+
+    displayName() {
+        return this.firstName;
+    }
+}
+
---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,67 @@
+interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+}
+
+class Director {
+    workFromHome() {
+        return 'Working from home';
+    }
+
+    getCoffeeBreak() {
+        return 'Getting a coffee break';
+    }
+
+    workDirectorTasks() {
+        return 'Getting to director tasks';
+    }
+}
+
+class Teacher {
+    workFromHome() {
+        return 'Cannot work from home';
+    }
+
+    getCoffeeBreak() {
+        return 'Cannot have a break';
+    }
+
+    workTeacherTasks() {
+        return 'Getting to work';
+    }
+}
+
+function createEmployee(salary: number | string): Director | Teacher {
+    if (typeof salary === 'number' && salary < 500) {
+        return new Teacher();
+    } else {
+        return new Director();
+    }
+}
+
+function isDirector(employee: Director | Teacher): employee is Director {
+    return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Director | Teacher): string {
+    if (isDirector(employee)) {
+        return employee.workDirectorTasks();
+    } else {
+        return employee.workTeacherTasks();
+    }
+}
+
+let Subjects = ['Math', 'History'];
+
+function teachClass(todayClass: string): string {
+    if (Subjects.indexOf(todayClass) > -1) {
+        return `Teaching ${todayClass}`;
+    }
+}
